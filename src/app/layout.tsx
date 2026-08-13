711d18204f678783d9b64d7fe66df0a9733afbf5
arga-nata <arganata.on@gmail.com>
2026-08-14 00:07:29 +0700

    feat(mobile): restrict zoom and text selection on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,78 +1,84 @@
 // src/app/layout.tsx
-import PwaRegister from "@/components/pwa-register";
-import type { Metadata,Viewport } from "next";
+import PwaRegister from "@/components/pwa-register"
+import type { Metadata, Viewport } from "next"
 // Kita ganti ke next/font/google
-import { Geist, Inter, Space_Grotesk, Montserrat, Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
+import {
+  Geist,
+  Inter,
+  Space_Grotesk,
+  Montserrat,
+  Plus_Jakarta_Sans,
+} from "next/font/google"
+import "./globals.css"
 
 // 1. GEIST (Default Sans)
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist",
   display: "swap",
-});
+})
 
 // 2. INTER
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
   display: "swap",
-});
+})
 
 // 3. SPACE GROTESK
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-space",
   display: "swap",
-});
+})
 
 // 4. MONTSERRAT
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
   display: "swap",
-});
+})
 
 // 5. PLUS JAKARTA
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
   variable: "--font-jakarta",
   display: "swap",
-});
+})
 
 export const metadata: Metadata = {
   title: "ARDEN",
   description: "Sistem Absensi Karakter Siswi",
- manifest: "/manifest.webmanifest",
+  manifest: "/manifest.webmanifest",
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
     title: "ARDEN",
   },
-};
+}
 
 export const viewport: Viewport = {
   themeColor: "#0a0e1a",
-};
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <body
-        className={`
-          ${geist.variable} 
-          ${inter.variable} 
-          ${spaceGrotesk.variable} 
-          ${montserrat.variable} 
-          ${jakarta.variable}
-          font-sans antialiased bg-[#000000] text-white selection:bg-indigo-500/30
-        `}
+        className={` ${geist.variable} ${inter.variable} ${spaceGrotesk.variable} ${montserrat.variable} ${jakarta.variable} bg-[#000000] font-sans text-white antialiased selection:bg-indigo-500/30`}
         suppressHydrationWarning
       >
-         <PwaRegister />
+        <PwaRegister />
         {children}
       </body>
     </html>
-  );
+  )
 }
